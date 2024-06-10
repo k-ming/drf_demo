2dd759a0744192307b2447e35399b317b9f588ad
zh88168 <zh88168@yeah.net>
2024-06-10 23:44:33 +0800

    使用viewset 重构视图， 使用coreapi 生成接口文档

--- flower/urls.py
+++ flower/urls.py
@@ -5,13 +5,16 @@
 # @Link    : https://k-ming.github.io/
 # @Version : $Id$
 
-from django.urls import path
+from django.urls import path, include
 from .views1 import FlowerListApiView, FlowerListDetailApiView
 from .import views2
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views3 import FlowerMixinsList, FlowerMixinsDetail,\
 					 FlowerMixedList, FlowerMixedDetail,\
 					 UserList, UserDetail
+from .viewset_view import UserViewSet, FlowerViewSet
+from rest_framework.schemas import get_schema_view   # 使用coreapi 来生成接口文档
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
 	path('list/', FlowerListApiView.as_view()),
@@ -23,8 +26,19 @@ urlpatterns = [
 	path('mixed_list/', FlowerMixedList.as_view()),
 	path('mixed_list/<int:pk>', FlowerMixedDetail.as_view()),
 	path('users/', UserList.as_view()),
-	path('users/<int:pk>', UserDetail.as_view())
+	path('users/<int:pk>', UserDetail.as_view()),
+	path('users_set/', UserViewSet.as_view({'get': 'list'})),
+	path('users_set/<int:pk>', UserViewSet.as_view({'get': 'retrieve'})),
+	path('doc/', schema_view)
 ]
 
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+# 使用路由器 router 绑定ModelViewSet视图
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r'', FlowerViewSet)
+urlpatterns +=[
+	path('model_list/', include(router.urls))
+]
